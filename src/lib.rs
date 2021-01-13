6ce6f5f2f602b328d75252c3b5099950ad0ee6fe
Jude <jdonmarie@gmail.com>
2021-01-13 22:42:08 +0100

    complete --output option

--- src/lib.rs
+++ src/lib.rs
@@ -23,13 +23,17 @@ pub fn matches<'a>() -> ArgMatches<'a>{
     .get_matches()
 }
 
+use std::io::Write;
 pub fn run(value: &str){
 
     let content = parse_file(value);
     let is_json = matches().value_of("output")
             .unwrap().contains(".json") ;
     if is_json{
-        check_valid_email(&content).unwrap();
+        let out = check_valid_email(&content).unwrap();
+        let mut file = fs::File::create(matches().value_of("output").unwrap()).expect("unable to create file");
+        
+        file.write_all(&out.as_bytes()).expect("could not write to file");
     }
 
     println!("input file {:?}", value);
@@ -68,22 +72,27 @@ fn check_valid_email(content: &str) -> Result<std::string::String, serde_json::E
     let mut vec: Vec<&str> = Vec::new();
     let mut cat: HashMap<&str, u32> = HashMap::new();
     let mut count = 0;
-    for line in content.lines() {
+    for line in content.lines().skip(1) {
 
         if re.is_match(line) {
             let word: Vec<&str> = line.split("@").collect();
             vec.push(word[1])
         }
-        count +=1;
+        if !line.trim().is_empty(){
+            count+=1
+        };
     }
 
     for item in vec.iter() {
         let count = cat.entry(item).or_insert(0);
         *count +=1
     }
+    let mut vec1 = vec.clone();
+    vec1.sort_unstable();
+    vec1.dedup();
 
     let obj = Object{
-        valid_domains: vec.clone(),
+        valid_domains: vec1,
         total_emails_parsed: count,
         total_valid_emails: vec.len() as u32,
         categories: cat
@@ -99,13 +108,32 @@ mod tests {
     
     
     #[test]
-    fn json_output(){
+    fn single_email_file(){
 
-       let content =  "jdon@gmail.com";
+       let content =  "Email
+       jdon@gmail.com";
         
         
         let out = "\
         {\"valid_domains\":[\"gmail.com\"],\"total_emails_parsed\":1,\"total_valid_emails\":1,\"categories\":{\"gmail.com\":1}}";
+
+        
+        assert_eq!(out, check_valid_email(content).unwrap())
+    }
+
+    #[test]
+    fn email_file_with_newline(){
+
+        let content =  "Emails
+        jdon@gmail.com
+        
+        hell0@gmail.com
+
+        hi@jumia
+        ";
+
+        let out = "\
+        {\"valid_domains\":[\"gmail.com\"],\"total_emails_parsed\":3,\"total_valid_emails\":2,\"categories\":{\"gmail.com\":2}}";
 
         
         assert_eq!(out, check_valid_email(content).unwrap())
